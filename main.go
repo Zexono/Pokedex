626ce5d46fcc7baea2d0db3bded4f4f3f7748d84
Zexono <spector6622@gmail.com>
2025-11-03 13:13:19 +0700

    simple REPL(read-eval-print loop)

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
+
 func main(){
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		user_input := scanner.Text()
+		command := cleanInput(user_input)
+		fmt.Println("Your command was: "+command[0])
+		
+	}
 }
 
 func cleanInput(text string) []string{
